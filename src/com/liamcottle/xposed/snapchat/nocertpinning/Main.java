a295b418b4b8322b1d8a92c197013dfdf15c6b43
Troy Cornwall <Furze@users.noreply.github.com>
2016-08-16 10:51:56 +1200

    Update Main.java
    Removed check for snapchat package, so that it now removes cert pinning for all android applications..

--- src/com/liamcottle/xposed/snapchat/nocertpinning/Main.java
+++ src/com/liamcottle/xposed/snapchat/nocertpinning/Main.java
@@ -10,18 +10,11 @@ public class Main implements IXposedHookLoadPackage {
 
     @Override
     public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-
-        if(lpparam.packageName.equals("com.snapchat.android")) {
-
-            XposedHelpers.findAndHookMethod(TrustManagerFactory.class, "init", KeyStore.class, new XC_MethodHook() {
-                @Override
-                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                    param.args[0] = null;
-                }
-            });
-
-        }
-
+        XposedHelpers.findAndHookMethod(TrustManagerFactory.class, "init", KeyStore.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                param.args[0] = null;
+            }
+        });
     }
-
 }
